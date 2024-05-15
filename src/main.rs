22f5e73e4a3039bfb4a26aeeb89ccfb5b1958507
steelPiber <pyh5523@gmail.com>
2024-05-15 15:03:03 +0900

    commit update 	수정함:        main.rs 	* 주석 정리

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ fn main() {
     // 인자를 받아서 처리
     let matches = arg::build_cli().get_matches();
 
-    let arg_num = matches.get_many::<String>("numbers").unwrap();
-    let (num1, num2) = numbers::parse_numbers(Some(arg_num.map(|s| s.to_string())));
-
+    let arg_num = matches.get_many::<String>("numbers").unwrap(); //인자값 가져옴
+    
+    //인자값을 파서하여 값을 num1, num2로 저장
+    let (num1, num2) = numbers::parse_numbers(Some(arg_num.map(|s| s.to_string()))); 
     //HashMap을 사용하여 플래그와 연산 함수를 매핑
     let arg_map: HashMap<&str, fn(i32, i32) -> i32> = HashMap::from([
         ("add", numbers::sum_numbers as fn(i32, i32) -> i32),
